ae1bec14ecc4d7fed1472e8c1ab54d56bcd48449
calandrajose <calandrajosei@gmail.com>
2021-01-04 13:27:54 -0300

    performance improvement

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import classes from './Modal.module.css'
-import Backdrop from '../Backdrop/Backdrop'
-import Aux from '../../../hoc/Auxiliary'
+import React, { Component } from "react";
+import classes from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 
-const modal = (props) => {
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate(){
+        console.log('[Modal] will updt');
+    }
+
+
+  render() {
     return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={classes.Modal} 
-            style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1':'0' }}>
-                {props.children}
-            </div>
-        </Aux>
- 
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
     );
-};
+  }
+}
 
-export default modal;
+export default Modal;
